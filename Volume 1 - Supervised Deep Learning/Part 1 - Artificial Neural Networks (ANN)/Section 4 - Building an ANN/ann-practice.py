4b8f19e13d7d1bc2346a1a8418fde9f2ed0a8873
AhmedSalahBasha <a.salah.b@gmail.com>
2020-03-22 21:54:36 +0100

    churn-classifier build ANN

---
+++ Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann-practice.py
@@ -0,0 +1,48 @@
+# Artifical Neural Network
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehot = pd.get_dummies(X[:, 1]).to_numpy()
+X = np.delete(X, 1, axis=1)
+X = np.concatenate([X, onehot], axis=1)
+X = X[:, :11]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Part 2 - building ANN
+
+# Importing Keras library 
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Intializing ANN
+classifier = Sequential()
+
+# Adding the input layer and first hidden layer
+classifier.add(Dense(output_dim = 6, init = 'uniform', activation = 'relu', input_dim = 11))
+
+
+# Part 3 - making predictions and evaluation
